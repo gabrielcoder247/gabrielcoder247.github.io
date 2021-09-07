b4b3a46539ee7ab7733169569bfdb9b8c072aa94
gabrielcoder247 <gabrielcoder247@gmail.com>
2021-09-07 08:56:47 +0100

    Fix linterc and eslintrc errors

--- script.js
+++ script.js
@@ -5,41 +5,35 @@ const menu = document.querySelector('.modal-container');
 const hamburgerTwo = document.getElementById('modal-harmburger');
 
 function openMenu() {
-
-    if (menu.classList.contains('invisible')) {
-        menu.classList.remove('invisible');
-        main.classList.add('invisible');
-    } else {
-        menu.classList.add('invisible');
-        main.classList.remove('invisible');
-    }
+  if (menu.classList.contains('invisible')) {
+    menu.classList.remove('invisible');
+    main.classList.add('invisible');
+  } else {
+    menu.classList.add('invisible');
+    main.classList.remove('invisible');
+  }
 }
-
 
 hambutton.addEventListener('click', openMenu);
 hamburgerTwo.addEventListener('click', openMenu);
 
 // scrollspy section
 
-
 const closebutton = document.querySelector('#modal-harmburger');
 
 function closeMenu() {
-    main.style.display = 'block';
-    menu.style.display = 'none';
+  main.style.display = 'block';
+  menu.style.display = 'none';
 }
 
 closebutton.addEventListener('click', closeMenu);
 
 const listItem = document.querySelectorAll('.menu-btn');
 listItem.forEach((item) => {
-    item.addEventListener('click', closeMenu);
+  item.addEventListener('click', closeMenu);
 });
 
-
 // console.log(menuSection)
-
-
 
 // // for window scrollspy event
 
