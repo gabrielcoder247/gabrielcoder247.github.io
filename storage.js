9d6e26fd0caa9e421c4648ee6b67d3e93f59b863
gabrielcoder247 <gabrielcoder247@gmail.com>
2021-09-11 11:50:46 +0100

    Make changes to email validation functions

--- storage.js
+++ storage.js
@@ -1,26 +1,24 @@
-function storeForm() {
-    const form = document.getElementById('form');
-    const name = form.name.value;
-    const email = form.email.value;
-    const comment = form.comment.value;
-    const myStorage = {
-        userName: name,
-        userEmail: email,
-        userComment: comment,
-    };
-    localStorage.setItem('storage', JSON.stringify(myStorage));
+const nameInput = document.getElementById('name');
+const emailInput = document.getElementById('email');
+const msgInput = document.getElementById('comment');
+const form = document.querySelector('form');
+
+if (localStorage.getItem('user') !== null) {
+  const objStore = JSON.parse(localStorage.getItem('user'));
+  nameInput.value = objStore.username;
+  emailInput.value = objStore.email;
+  msgInput.value = objStore.msg;
 }
-function refresh() {
-    if (localStorage.getItem('storage')) {
-      const oldStorage = localStorage.getItem('storage');
-      const newStorage = JSON.parse(oldStorage);
-      const form = document.getElementById('form');
-      form.name.value = newStorage.userName;
-      form.email.value = newStorage.userEmail;
-      form.comment.value = newStorage.userComment;
-    }
+
+form.addEventListener('input', () => {
+  const obj = {
+    username: nameInput.value,
+    email: emailInput.value,
+    msg: msgInput.value,
+  };
+  if (typeof (Storage) !== 'undefined') {
+    localStorage.setItem('user', JSON.stringify(obj));
+  } else {
+    document.getElementById('result').innerHTML = 'Sorry, your browser does not support Web Storage...';
   }
-  
-  window.onload = refresh;
-  
-  storeForm();
+});
